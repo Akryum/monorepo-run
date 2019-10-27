5bd62223b3eefb796869784a9f0d4600a911721b
Guillaume Chau <guillaume.b.chau@gmail.com>
2019-10-27 21:53:57 +0100

    fix: windows spawn error

--- src/bin/mono-run.js
+++ src/bin/mono-run.js
@@ -5,11 +5,12 @@ const path = require('path')
 const nodeCleanup = require('node-cleanup')
 const ansiEscapes = require('ansi-escapes')
 const { terminate } = require('../util/terminate')
+const { isLinux } = require('../util/platform')
 
-const child = cp.spawn(path.join(__dirname, 'child-mono-run.js'), process.argv.slice(2), {
+const child = cp.fork(path.join(__dirname, 'child-mono-run.js'), process.argv.slice(2), {
   stdio: 'inherit',
   cwd: process.cwd(),
-  detached: true,
+  detached: isLinux,
 })
 
 nodeCleanup(() => {
